fefb5ca11e05289433ad43a41d34367dd437501c
kaman097 <contactamankumar1991@gmail.com>
2022-04-25 01:27:16 +0530

    created redux store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import React from 'react';
 
-function App() {
-  return (
+class App extends React.Component{
+
+  render(){
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.props.age}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={this.props.onAgeUp}>increase</button>
+        <button onClick={this.props.onAgeDown}>decrease</button>
       </header>
     </div>
-  );
+  )
+};
 }
 
-export default App;
+const mapStateToProps=(state)=>{
+  return {age: state.age}
+}
+
+const mapDispatchToProps=(dispatch)=>{
+return{
+  onAgeUp:()=>dispatch({type:'AGEUP'}),
+  onAgeDown:()=>dispatch({type:"AGEDOWN"})
+}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
